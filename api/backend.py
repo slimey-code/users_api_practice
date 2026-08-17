4f561ff88fc4d4a3ec06f4618888fab1938c4f50
slimey-code <artemkacherkassy@gmail.com>
2026-08-17 22:10:14 +0300

    commit 4.

--- api/backend.py
+++ api/backend.py
@@ -3,7 +3,7 @@ from flask import Flask,render_template,request
 users = []
 
 
-app = Flask(__name__, template_folder="../templates")
+app = Flask(__name__, template_folder='../templates')
 
 @app.route("/")
 def home():
